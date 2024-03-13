4c7ec2458ca224aa5369d4f4dd263ee4fabf6d95
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-03-13 23:58:22 +0200

    [INIT] java springboot

---
+++ src/main/java/com/ht/listing_microservice/ListingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ht.listing_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListingMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ht/listing_microservice/ListingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ht.listing_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
