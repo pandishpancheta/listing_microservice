6d6977b04707e72c2f084a9c882f91d411066224
Martin Bozhilov <marogo142005@gmail.com>
2024-03-14 19:01:53 +0200

    [REFACTOR] switch from java to GO

--- src/main/java/com/ht/listing_microservice/repository/FavouritesRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ht.listing_microservice.repository;
-
-import com.ht.listing_microservice.entity.Favourites;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavouritesRepository extends JpaRepository<Favourites, Long> { }
