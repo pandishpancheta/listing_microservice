6118a195c4ed44242ad8d2acb7eff6f28d681b93
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2024-03-14 15:34:11 +0200

    Change mappers

--- src/main/java/com/ht/listing_microservice/mapper/FavouritesMapper.java
+++ src/main/java/com/ht/listing_microservice/mapper/FavouritesMapper.java
@@ -3,6 +3,7 @@ package com.ht.listing_microservice.mapper;
 import com.ht.listing_microservice.dto.FavouritesDTO;
 import com.ht.listing_microservice.entity.Favourites;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -12,9 +13,11 @@ public interface FavouritesMapper {
 
     FavouritesMapper INSTANCE = Mappers.getMapper(FavouritesMapper.class);
 
+    @Mapping(source = "id", target = "listingId")
     FavouritesDTO toDTO(Favourites favourites);
 
-    List<FavouritesDTO> toDTOs(List<Favourites> favourites);
-
+    @Mapping(source = "listingId", target = "id")
     Favourites toEntity(FavouritesDTO favouritesDTO);
+
+    List<FavouritesDTO> toDTOs(List<Favourites> favourites);
 }
--- src/main/java/com/ht/listing_microservice/mapper/ListingMapper.java
+++ src/main/java/com/ht/listing_microservice/mapper/ListingMapper.java
@@ -2,38 +2,28 @@ package com.ht.listing_microservice.mapper;
 
 import com.ht.listing_microservice.dto.ListingDTO;
 import com.ht.listing_microservice.entity.Listing;
-import com.ht.listing_microservice.entity.Tag;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper
 public interface ListingMapper {
-
     ListingMapper INSTANCE = Mappers.getMapper(ListingMapper.class);
 
-    @Mapping(source = "tags", target = "tags", qualifiedByName = "tagsToStringArray")
+    @Mappings({
+            @Mapping(source = "status", target = "status")
+    })
     ListingDTO toDTO(Listing listing);
 
-    @Mapping(source = "tags", target = "tags", qualifiedByName = "stringArrayToTags")
+    @Mappings({
+            @Mapping(source = "status", target = "status")
+    })
     Listing toEntity(ListingDTO listingDTO);
 
-    @Mapping(source = "tags", target = "tags", qualifiedByName = "tagsToStringArray")
+
     List<ListingDTO> toDTOs(List<Listing> listings);
-
-    default String[] tagsToStringArray(List<Tag> tags) {
-        return tags.stream().map(Tag::getName).toArray(String[]::new);
-    }
-
-    default List<Tag> stringArrayToTags(String[] tags) {
-        return tags == null ? null : Arrays.stream(tags).map(tag -> {
-            Tag t = new Tag();
-            t.setName(tag);
-            return t;
-        }).collect(Collectors.toList());
-    }
 }
